9f4a36d575f98637c30f959d8c285532eb5cd509
Noah Harris <dakoolstwunn@gmail.com>
2020-12-28 00:07:47 -0500

    Make groudon app

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/gastrodon/scyther
 go 1.15
 
 require (
-	github.com/gastrodon/groudon v1.0.0 // indirect
+	github.com/gastrodon/groudon v1.0.0
+	github.com/go-sql-driver/mysql v1.5.0
 	github.com/mitchellh/mapstructure v1.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"github.com/gastrodon/groudon"
+	"github.com/gastrodon/scyther/api"
+	"github.com/gastrodon/scyther/storage"
 
+	"net/http"
+	"os"
+)
+
+func main() {
+	storage.Connect(os.Getenv("SCYTHER_CONNECTION"))
+
+	groudon.RegisterHandler("GET", "^/queues/?$", api.GetQueues)
+
+	http.HandleFunc("/", groudon.Route)
+	http.ListenAndServe(":8000", nil)
 }
