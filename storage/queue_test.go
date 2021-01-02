369da90d549f68922c772a755ac9ae071353b045
Noah Harris <mail@gastrodon.io>
2021-01-02 16:48:59 -0500

    Add storage tests

--- storage/queue_test.go
+++ storage/queue_test.go
@@ -2,6 +2,7 @@ package storage
 
 import (
 	"github.com/gastrodon/scyther/types"
+	"github.com/google/uuid"
 
 	"testing"
 )
@@ -11,13 +12,7 @@ func Test_ReadQueues(test *testing.T) {
 
 	var index, population int = 0, 10
 	for index != population {
-		var queue types.QueuePost = types.QueuePost{
-			Name:      nil,
-			Capacity:  nil,
-			Ephemeral: false,
-		}
-
-		_, _ = WriteQueue(queue)
+		_, _ = database.Exec(WRITE_QUEUE, uuid.New().String(), nil, nil)
 		index++
 	}
 
@@ -34,5 +29,151 @@ func Test_ReadQueues(test *testing.T) {
 
 	if count != len(queues) {
 		test.Fatalf("inaccurate count: %d != %d", count, len(queues))
+	}
+}
+
+func Test_ReadQueues_empty(test *testing.T) {
+	test.Cleanup(Clear)
+
+	var population int = 0
+	var queues []types.QueueGet
+	var count int
+	var err error
+	if queues, count, err = ReadQueues(); err != nil {
+		test.Fatal(err)
+	}
+
+	if count != population {
+		test.Fatalf("incorrect count: %d != %d", count, population)
+	}
+
+	if count != len(queues) {
+		test.Fatalf("inaccurate count: %d != %d", count, len(queues))
+	}
+}
+
+func Test_ReadQueue(test *testing.T) {
+	test.Cleanup(Clear)
+
+	var id string = uuid.New().String()
+	_, _ = database.Exec(WRITE_QUEUE, id, nil, nil)
+
+	var queue types.QueueGet
+	var exists bool
+	var err error
+	if queue, exists, err = ReadQueue(id); err != nil {
+		test.Fatal(err)
+	}
+
+	if !exists {
+		test.Fatalf("queue %s doesn't exist", id)
+	}
+
+	if queue.ID != id {
+		test.Fatalf("id incorrect, %s != %s", queue.ID, id)
+	}
+}
+
+func Test_ReadQueue_noSuchQueue(test *testing.T) {
+	var exists bool
+	var err error
+	if _, exists, err = ReadQueue(uuid.New().String()); err != nil {
+		test.Fatal(err)
+	}
+
+	if exists {
+		test.Fatalf("queue of random id exists")
+	}
+}
+
+func Test_ResolveNameId(test *testing.T) {
+	test.Cleanup(Clear)
+
+	var id string = uuid.New().String()
+	var name string = "resolve_me"
+	_, _ = database.Exec(WRITE_QUEUE, id, &name, nil)
+
+	var resolvedId string
+	var exists bool
+	var err error
+	if resolvedId, exists, err = ResolveNameId(name); err != nil {
+		test.Fatal(err)
+	}
+
+	if !exists {
+		test.Fatalf("queue %s doesn't exist", name)
+	}
+
+	if resolvedId != id {
+		test.Fatalf("incorrect id, %s != %s", resolvedId, id)
+	}
+}
+
+func Test_ResolveNameId_noSuchQueue(test *testing.T) {
+	var exists bool
+	var err error
+	if _, exists, err = ResolveNameId(uuid.New().String()); err != nil {
+		test.Fatal(err)
+	}
+
+	if exists {
+		test.Fatalf("queue of random id exists")
+	}
+}
+
+func Test_WriteQueue(test *testing.T) {
+	test.Cleanup(Clear)
+
+	var id string
+	var err error
+	if id, err = WriteQueue(types.QueuePost{nil, nil, false}); err != nil {
+		test.Fatal(err)
+	}
+
+	var exists bool
+	if err = database.QueryRow(TEST_READ_QUEUE_ID_EXISTS, id).Scan(&exists); err != nil {
+		test.Fatal(err)
+	}
+
+	if !exists {
+		test.Fatalf("queue %s doesn't exist", id)
+	}
+}
+
+func Test_WriteQueue_nameConflict(test *testing.T) {
+	test.Cleanup(Clear)
+
+	var name string = "conflict_me"
+	var err error
+	if _, err = WriteQueue(types.QueuePost{&name, nil, false}); err != nil {
+		test.Fatal(err)
+	}
+
+	if _, err = WriteQueue(types.QueuePost{&name, nil, false}); err == nil {
+		test.Fatalf("No error for duplicate queue %s", name)
+	}
+}
+
+func Test_DeleteQueue(test *testing.T) {
+	test.Cleanup(Clear)
+
+	var id string = uuid.New().String()
+	_, _ = database.Exec(WRITE_QUEUE, id, nil, nil)
+
+	var err error
+	if err = DeleteQueue(id); err != nil {
+		test.Fatal(err)
+	}
+
+	var exists bool
+	if database.QueryRow(TEST_READ_QUEUE_ID_EXISTS).Scan(&exists); exists {
+		test.Fatalf("queue %s still exists", id)
+	}
+}
+
+func Test_DeleteQueue_noSuchQueue(test *testing.T) {
+	var err error
+	if err = DeleteQueue(uuid.New().String()); err != nil {
+		test.Fatal(err)
 	}
 }
