c0a539751212f622e1cf24420db3b9d973c5ffe1
Noah Harris <mail@gastrodon.io>
2021-01-02 04:22:15 -0500

    Move uuid regex into regular api
    Move uuid regex stuff from the testing part of package api into the
    regular runtime package

--- api/api.go
+++ api/api.go
@@ -1,7 +1,20 @@
 package api
 
+import (
+	"regexp"
+)
+
+const (
+	UUID_PATTERN = `^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$`
+)
+
 var (
+	uuidRegex *regexp.Regexp = regexp.MustCompile(UUID_PATTERN)
+
 	keyQueue = key("queue")
+
+	targetNotFound map[string]interface{} = map[string]interface{}{"error": "no_such_queue"}
+	badRequest     map[string]interface{} = map[string]interface{}{"error": "bad_request"}
 )
 
 type key string
--- api/api_test.go
+++ api/api_test.go
@@ -11,16 +11,7 @@ import (
 	"io"
 	"net/http"
 	"os"
-	"regexp"
 	"testing"
-)
-
-const (
-	UUID_PATTERN = `^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$`
-)
-
-var (
-	uuidRegex *regexp.Regexp = regexp.MustCompile(UUID_PATTERN)
 )
 
 func TestMain(main *testing.M) {
