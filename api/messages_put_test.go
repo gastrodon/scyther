8e0e7f9a12be3f921f07289c42eefdec8c470476
Noah Harris <mail@gastrodon.io>
2021-01-07 14:38:37 -0500

    Add put tests for mismatch between sizes
    Add tests to see what PutMessage will do if the message doesn't match
    the content length

--- api/messages_put_test.go
+++ api/messages_put_test.go
@@ -5,10 +5,13 @@ import (
 	"github.com/gastrodon/scyther/types"
 
 	"bytes"
+	"net/http"
 	"testing"
 )
 
 func Test_PutMessage(test *testing.T) {
+	test.Cleanup(storage.Clear)
+
 	var id string
 	id, _ = storage.WriteQueue(types.QueuePost{nil, nil, false})
 
@@ -38,7 +41,61 @@ func Test_PutMessage(test *testing.T) {
 	messageOk(message, messageFetched, test)
 }
 
+func Test_PutMessage_tooLong(test *testing.T) {
+	test.Cleanup(storage.Clear)
+
+	var id string
+	id, _ = storage.WriteQueue(types.QueuePost{nil, nil, false})
+
+	var message, partial string = "foobar", "foo"
+	var data *bytes.Buffer = bytes.NewBufferString(message)
+	var size int64 = 3
+	var request *http.Request = newRequestForQueue("PUT", "/queues/"+id, data, id)
+	request.ContentLength = size
+
+	var code int
+	var err error
+	if code, _, err = PutMessage(request); err != nil {
+		test.Fatal(err)
+	}
+
+	codeOk(code, 204, test)
+
+	var messageFetched string
+	messageFetched, _, _ = storage.ReadHead(id)
+
+	messageOk(partial, messageFetched, test)
+}
+
+func Test_PutMessage_tooShort(test *testing.T) {
+	test.Cleanup(storage.Clear)
+
+	var id string
+	id, _ = storage.WriteQueue(types.QueuePost{nil, nil, false})
+
+	var message string = "foobar"
+	var data *bytes.Buffer = bytes.NewBufferString(message)
+	var size int64 = 9
+	var request *http.Request = newRequestForQueue("PUT", "/queues/"+id, data, id)
+	request.ContentLength = size
+
+	var code int
+	var err error
+	if code, _, err = PutMessage(request); err != nil {
+		test.Fatal(err)
+	}
+
+	codeOk(code, 204, test)
+
+	var messageFetched string
+	messageFetched, _, _ = storage.ReadHead(id)
+
+	messageOk(message, messageFetched, test)
+}
+
 func Test_PutMessage_pastCapacity(test *testing.T) {
+	test.Cleanup(storage.Clear)
+
 	var capacity int = 0
 	var id string
 	id, _ = storage.WriteQueue(types.QueuePost{nil, &capacity, false})
